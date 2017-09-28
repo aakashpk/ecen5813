de38cca8a1c19b1730710a94e61a7a89ec5543b0
Aakash <Aakash.Kumar@colorado.edu>
2017-09-28 16:27:03 -0600

    Created folder structure as described in the project description. Working code for debug.c

--- Proj1/include/common/debug.h
+++ Proj1/include/common/debug.h
@@ -1,8 +1,10 @@
 #ifndef DEBUG_H
 #define DEBUG_H
-#include<stdio.h>
 
-void print_memory(uint8_t * start, uint32_t length)
+#include<stdio.h>
+#include<stdint.h>
+
+void print_memory(uint8_t * start, uint32_t length);
 /*
 @brief function to print contents of a block of memory
 @param * start byte pointer to the source address of memory block to be printed
---
+++ Proj1/src/sources.c
@@ -0,0 +1,10 @@
+#include "debug.h"
+
+int main()
+{
+
+uint8_t a[]={0,1,2,3,4,5,6};
+print_memory(a,7);
+printf("\n");
+return 0;
+}
