16f907697fd069d517eb6baa482c7007d5429724
Aakash <Aakash.Kumar@colorado.edu>
2017-10-04 14:20:34 -0600

    Generating target for KL25Z, updated file locations, changes to make

--- Proj1/src/main.c
+++ Proj1/src/main.c
@@ -11,22 +11,22 @@ int main()
 
 //uint8_t a[]="-2FD";
 
-//uint8_t *b=malloc(10),c;
+uint8_t *b=malloc(10),c;
 //int32_t d;
 
-uint32_t e[]={0xAA00CC15,0x90AB12CD};
+//uint32_t e[]={0xAA00CC15,0x90AB12CD};
 
-//c=my_itoa(-765,b,2);
+c=my_itoa(-765,b,2);
 
-//print_memory(b,c);
+print_memory(b,c);
 
 //printf("\n %s \n",b);
 
 //d=my_atoi(a,4,16);
-int i=0;
+//int i=0;
 //printf("\n %d \n",d);
-print_memory(e,2);
-for(i=0;i<2;i++)
+//print_memory(e,2);
+/*for(i=0;i<2;i++)
 {
 	printf("-%x\n",*(e+i));
 }
@@ -37,6 +37,7 @@ for(i=0;i<2;i++)
 {
 	printf("--%x\n",*(e+i));
 }
-print_memory(e,2);
+*/
+//print_memory(e,2);
 return 0;
 }
